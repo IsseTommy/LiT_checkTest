5b3780b2a047ba1a8abaf1631b25843ce7955ca3
IsseTommy <0jofePfN>
2019-04-14 15:13:04 +0900

    Initial Commit

---
+++ checkTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  checkTest
+//
+//  Created by Tommy on 2019/04/14.
+//  Copyright © 2019 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
